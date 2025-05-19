9e0e2579c890a5cb1d20b53f07a932fc2c4b5adf
Santosh Kumar Areeboyena <santoshareeboyena@gmail.com>
2025-05-19 12:16:47 +0100

    Day4 push

---
+++ day4.py
@@ -0,0 +1,24 @@
+todos = []
+
+while True:
+    user_action = input("Type your action: 1 to ADD, 2 to SHOW, 3 to EDIT or 4 to QUIT:")
+    user_action.strip()
+    print(user_action)
+    match user_action:
+        case "1":
+            todos.append(input("Enter a todo:"))
+        case "2":
+            for item in todos:
+                print(item)
+        case "3":
+            for item in todos:
+                print(item)
+            number=int(input("Which todo to edit:"))
+            number = number-1
+            new_todo = input("Enter the new todo value:")
+            todos[number]=new_todo
+            print("updated list is as below")
+            for item in todos:
+                print(item)
+        case "4":
+            break
