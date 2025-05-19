fc57f485795f449c293d51f27904e2b9001ca00e
Santosh Kumar Areeboyena <santoshareeboyena@gmail.com>
2025-05-19 11:23:59 +0100

    Day1 push

--- day1.py
+++ day1.py
@@ -1,1 +1,9 @@
 ''' hi! this is a sample git push '''
+user_todo="What do you want to enter bro?"
+todo1= input(user_todo)
+todo2= input(user_todo)
+todo3= input(user_todo)
+todos= [todo1, todo2, todo3, "Hello User"]
+print(todos)
+print(f"type of todos is {type(todos)}")
+print(f"type of todo activity is {type(todo1)}")
